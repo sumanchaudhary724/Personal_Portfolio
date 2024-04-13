fc219587a309746b3217fcac556cd2b39fc55eba
SumanChaudhary <sumanchaudhary724@gmail.com>
2024-04-13 14:52:28 +1000

    Added icons

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -24,6 +24,33 @@ export const Skills = () => {
               <div>
                 <i className="fa-brands fa-figma text-info"></i> Figma
               </div>
+              <div>
+                <i className="fa-solid fa-laptop text-success"></i> Hardware &
+                Network
+              </div>
+              <div>
+                <i className="fa-brands fa-windows text-info"></i> Operating
+                System
+              </div>
+              <div>
+                <i className="fa-brands fa-cloud text-primary"></i> Cloud
+                Technologies
+              </div>
+              <div>
+                <i className="fa-solid fa-database text-warning"></i> Backup
+                Technologies
+              </div>
+              <div>
+                <i className="fa-solid fa-briefcase text-danger"></i>{" "}
+                Applications
+              </div>
+              <div>
+                <i className="fa-solid fa-network-wired text-success"></i>{" "}
+                Networking
+              </div>
+              <div>
+                <i className="fa-solid fa-ticket-alt text-info"></i> Ticketing
+              </div>
             </div>
           </div>
         </div>
