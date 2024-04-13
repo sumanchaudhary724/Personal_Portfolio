c524037f387338ebecafe3bf0d5f26c0690165ab
SumanChaudhary <sumanchaudhary724@gmail.com>
2024-04-13 15:02:18 +1000

    Updated icons row col

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -8,49 +8,50 @@ export const Skills = () => {
               <span>Skills</span>
             </div>
             <div className="icons row">
-              <div className="col-sm-6 col-md-4">
+              <div className="col-4 col-md-2">
                 <i className="fa-brands fa-html5 text-danger"></i> HTML
               </div>
-              <div className="col-sm-6 col-md-4">
+              <div className="col-4 col-md-2">
                 <i className="fa-brands fa-css3-alt text-primary"></i> CSS
               </div>
-              <div className="col-sm-6 col-md-4">
+              <div className="col-4 col-md-2">
                 <i className="fa-brands fa-square-js text-warning"></i>{" "}
                 JavaScript
               </div>
-              <div className="col-sm-6 col-md-4">
+              <div className="col-4 col-md-2">
                 <i className="fa-brands fa-react"></i> React
               </div>
-              <div className="col-sm-6 col-md-4">
+              <div className="col-4 col-md-2">
                 <i className="fa-brands fa-figma text-info"></i> Figma
               </div>
-              <div className="col-sm-6 col-md-4">
+              <div className="col-4 col-md-2">
                 <i className="fa-solid fa-laptop text-success"></i> Hardware &
                 Network
               </div>
-              <div className="col-sm-6 col-md-4">
+              <div className="col-4 col-md-2">
                 <i className="fa-brands fa-windows text-info"></i> Operating
                 System
               </div>
-              <div className="col-sm-6 col-md-4">
+              <div className="col-4 col-md-2">
                 <i className="fa-brands fa-cloud text-primary"></i> Cloud
                 Technologies
               </div>
-              <div className="col-sm-6 col-md-4">
+              <div className="col-4 col-md-2">
                 <i className="fa-solid fa-database text-warning"></i> Backup
                 Technologies
               </div>
-              <div className="col-sm-6 col-md-4">
+              <div className="col-4 col-md-2">
                 <i className="fa-solid fa-briefcase text-danger"></i>{" "}
                 Applications
               </div>
-              <div className="col-sm-6 col-md-4">
+              <div className="col-4 col-md-2">
                 <i className="fa-solid fa-network-wired text-success"></i>{" "}
                 Networking
               </div>
-              <div className="col-sm-6 col-md-4">
+              <div className="col-4 col-md-2">
                 <i className="fa-solid fa-ticket-alt text-info"></i> Ticketing
               </div>
+              {/* Additional icons can be added here */}
             </div>
           </div>
         </div>
